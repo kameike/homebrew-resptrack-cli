f1d181ff13a87b817f0ae1cf12ff88392a2e6d8c
kameike <kameike@users.noreply.github.com>
2020-06-15 03:28:35 +0900

    update to v0.2.0

--- resptrack-cli.rb
+++ resptrack-cli.rb
@@ -1,12 +1,12 @@
 class ResptrackCli < Formula
   homepage 'https://github.com/kameike/resptrack'
-  version '0.1.5'
+  version '0.2.0'
   if OS.linux?
-    url 'https://github.com/kameike/homebrew-resptrack-cli/releases/download/v0.1.5/resptrack-cli_v0.1.5_linux_amd64.tar.zip'
-    sha256 '966344ea493f3b768600563ab13d8e4174618f7d3fc12faf95e6ce73b8f9c457'
+    url 'https://github.com/kameike/homebrew-resptrack-cli/releases/download/v0.2.0/resptrack-cli_v0.2.0_linux_amd64.tar.zip'
+    sha256 'da6764a027fb473bc83c6876c2399c288e1e8063ad9b6d9dc0f90acc4be41f2c'
   else
-    url 'https://github.com/kameike/homebrew-resptrack-cli/releases/download/v0.1.5/resptrack-cli_v0.1.5_darwin_amd64.zip'
-    sha256 'a544a64f99811e6432c155b8d39042629f12db2535579970f359c3d01a0ed9ad'
+    url 'https://github.com/kameike/homebrew-resptrack-cli/releases/download/v0.2.0/resptrack-cli_v0.2.0_darwin_amd64.zip'
+    sha256 'aef916539e73e7fbb38b438d367ac2b749d8f425324f7b1b1ba620d62e01a07e'
   end
   def install
     bin.install 'resptrack-cli'
